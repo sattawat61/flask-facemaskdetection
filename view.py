09d680ebf6db9a480d1ded44b6b898d60bc92e59
sattawat61 <s6104062616163@email.kmutnb.ac.th>
2022-04-20 13:02:56 +0700

    v.0.0.2

--- view.py
+++ view.py
@@ -1,9 +1,36 @@
-from flask import Flask,render_template
+from tkinter import Frame
+from flask import Flask,render_template,Response
+import cv2
+import tensorflow as tf
+import numpy as np
 from Member import *
 from User import *
 from datetime import timedelta
+###############
+from facemaskdetection_2 import stream
+
+
 
 app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+
+def gen_frame():
+    """Video streaming generator function."""
+    while True:
+        frame = stream()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') # concate frame one by one and show result
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen_frame(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+
+
 app.secret_key = "sattawat"
 app.permanent_session_lifetime = timedelta(days=1)
 app.register_blueprint(member)
@@ -13,6 +40,16 @@ app.register_blueprint(user)
 def Index():
     # return "This is home"
     return render_template('login.html',headername="Login เข้าใช้งานระบบ")
+
+@app.route("/test")
+def index1():
+    return render_template('test.html',)
+
+@app.route("/video")
+def video():
+    return Response(gen_frame(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 
 if __name__ == '__main__':
     app.run(debug = True)
